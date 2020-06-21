6c8c2e7eb045a7744c8f3a9cd81aaff7d4dc6f62
Alekhya Mandala <alekhya.mandala99@gmail.com>
2020-06-21 21:37:16 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data/data.json'
+import {BrowserRouter,Route,Link} from 'react-router-dom';
+import Reac_resume from './Reac_resume';
+import logo from './profile_pic.svg';
+import './css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Route exact path="/" component={Home}></Route>
+    <Route exact path="/resume" component={Reac_resume}></Route>
+    </BrowserRouter>
   );
 }
+
+let Home=()=>
+ {
+   let  profiles=data.profile;
+   console.log(profiles);
+   return( 
+   <div>
+         <div className="row justify-content-center">
+             {profiles.map((values,index)=>(
+             <div className="col-lg-4 col-md-4 col-sm-10 mt-2" key={index}>
+                <div className="card bg-secondary">
+                   <div className="card-body text-center " >
+                     <img src={logo} style={{width:"30%"}}></img>
+                     <h3>{values.basics.name}</h3>
+                     <hr/>
+                    
+                     <a href={"tel:"+ values.basics.Mobile}>
+                     <p className="text-dark">{values.basics.Mobile}</p></a>
+                     <a href={"mailto:"+ values.basics.email}>
+                     <p className="text-dark">{values.basics.email}</p></a>
+                     <p className="">{values.basics.pin}</p>
+
+                     <Link to={{ pathname:"/resume",data:{id:index}}} className="btn btn-dark text-white">View Profile</Link>
+                   </div>
+                 </div>
+              </div>
+         ))}
+         </div>
+   </div>
+   )
+ }
 
 export default App;
